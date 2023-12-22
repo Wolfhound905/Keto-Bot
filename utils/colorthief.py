2f443a3094e819107144450c1d4c7c9ab1c7c715
stekc <me@stkc.win>
2023-12-21 23:49:04 -0800

    More color fetching improvements

--- utils/colorthief.py
+++ utils/colorthief.py
@@ -1,20 +1,39 @@
-import io, re
+import io, re, os
 import fast_colorthief
 from aiocache import cached
-from urllib.request import urlopen, Request
+from aiohttp import ClientSession, ClientTimeout
 
 
 @cached(ttl=604800)
 async def get_color(query):
+    quality = os.getenv("COLOR_FETCH_QUALITY", default="off")
+
+    if quality == "off":
+        return 0x505050
+
     try:
-        # speed up color fetching for discord avatars
-        if any(s in query for s in {'cdn.discordapp.com/icons/', 'cdn.discordapp.com/avatars/'}):
-            query = re.sub(r'\?size=(32|64|128|256|512|1024|2048|4096)$', '?size=16', query)
-        req = Request(query)
-        req.add_header("User-Agent", "Keto - stkc.win")
-        content = urlopen(req, timeout=5).read()
+        # Speed up color fetching for discord avatars
+        if any(
+            s in query
+            for s in {"cdn.discordapp.com/icons/", "cdn.discordapp.com/avatars/"}
+        ):
+            if quality == "low":
+                query = re.sub(
+                    r"\?size=(32|64|128|256|512|1024|2048|4096)$", "?size=16", query
+                )
+            elif quality == "medium":
+                query = re.sub(
+                    r"\?size=(16|32|128|256|512|1024|2048|4096)$", "?size=64", query
+                )
+            elif quality == "high":
+                query = re.sub(
+                    r"\?size=(16|32|64|256|512|1024|2048|4096)$", "?size=128", query
+                )
+        async with ClientSession() as session:
+            async with session.get(query, timeout=5) as response:
+                content = await response.read()
+
         color = fast_colorthief.get_dominant_color(io.BytesIO(content), quality=100)
         return color
     except:
-        color = 0x505050
-        return color
+        return 0x505050
